2b6161f5a7cbabf510ad952c7f0427adb6a375f1
aabrittingham <aabrittingham@users.noreply.github.com>
2021-03-11 22:45:48 -0500

    updated with new info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Run from "./components/Run";
 import JumpRope from "./components/JumpRope";
+import Weights from "./components/Weights";
+import Swimming from "./components/Swimming";
 function App() {
   return (
     <div>
       <h1>Exercise Tracker</h1>
       <Run />
       <JumpRope />
+      <Weights />
+      <Swimming />
     </div>
   );
 }
